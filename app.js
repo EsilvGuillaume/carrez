d6a67132164584f49004adf27cce06a5d304896f
EsilvGuillaume <guillaume.ambroise@devinci.fr>
2017-02-08 04:04:18 +0100

    Create arborescence first template, first blank file

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+var bodyparser = require('body-parser');
+app.use("/Style", express.static( __dirname + '/Style'));
+app.use("/Js", express.static( __dirname + '/Js'));
 
 app.get('/', function (req, res) {
-  res.send("Carrez app");
+  res.sendfile( __dirname + "/Templates/Accueil.html");
 });
-
+app.get('/Accueil', function (req,res) {
+  res.sendfile( __dirname + "/Templates/Accueil.html");
+});
+app.get('/Calculate', function (req, res) {
+  res.sendfile( __dirname + "/Templates/Calculate.html");
+});
 app.listen(3000, function () {
   console.log('Carrez app available on http://localhost:3000 !');
 });
-
